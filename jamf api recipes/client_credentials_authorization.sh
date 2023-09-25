74d2f7c71b2b5a9752b4cdfc0375158e9aea4a8f
Jonathan Porter <38440687+jp-cpe@users.noreply.github.com>
2023-09-25 15:37:55 -0700

    Create client_credentials_authorization.sh

---
+++ jamf api recipes/client_credentials_authorization.sh
@@ -0,0 +1,46 @@
+url="https://yourserver.jamfcloud.com"
+client_id="your-client-id"
+client_secret="yourClientSecret"
+
+getAccessToken() {
+	response=$(curl --silent --location --request POST "${url}/api/oauth/token" \
+ 	 	--header "Content-Type: application/x-www-form-urlencoded" \
+ 		--data-urlencode "client_id=${client_id}" \
+ 		--data-urlencode "grant_type=client_credentials" \
+ 		--data-urlencode "client_secret=${client_secret}")
+ 	access_token=$(echo "$response" | plutil -extract access_token raw -)
+ 	token_expires_in=$(echo "$response" | plutil -extract expires_in raw -)
+ 	token_expiration_epoch=$(($current_epoch + $token_expires_in - 1))
+}
+
+checkTokenExpiration() {
+ 	current_epoch=$(date +%s)
+    if [[ token_expiration_epoch -ge current_epoch ]]
+    then
+        echo "Token valid until the following epoch time: " "$token_expiration_epoch"
+    else
+        echo "No valid token available, getting new token"
+        getAccessToken
+    fi
+}
+
+invalidateToken() {
+	responseCode=$(curl -w "%{http_code}" -H "Authorization: Bearer ${access_token}" $url/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
+	if [[ ${responseCode} == 204 ]]
+	then
+		echo "Token successfully invalidated"
+		access_token=""
+		token_expiration_epoch="0"
+	elif [[ ${responseCode} == 401 ]]
+	then
+		echo "Token already invalid"
+	else
+		echo "An unknown error occurred invalidating the token"
+	fi
+}
+
+checkTokenExpiration
+curl -H "Authorization: Bearer $access_token" $url/api/v1/jamf-pro-version -X GET
+checkTokenExpiration
+invalidateToken
+curl -H "Authorization: Bearer $access_token" $url/api/v1/jamf-pro-version -X GET
