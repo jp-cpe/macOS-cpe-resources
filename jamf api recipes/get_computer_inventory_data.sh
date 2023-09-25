5255515d9e0fcf8a92cc3851fe911de257d06ec4
Jonathan Porter <38440687+jp-cpe@users.noreply.github.com>
2023-09-25 15:40:40 -0700

    Create get_computer_inventory_data.sh

---
+++ jamf api recipes/get_computer_inventory_data.sh
@@ -0,0 +1,10 @@
+url="https://yourserver.jamfcloud.com"
+bearerToken="BearerTokenValue"
+sections="GENERAL,HARDWARE" ###Supported Values are: GENERAL, DISK_ENCRYPTION, PURCHASING, APPLICATIONS, STORAGE, USER_AND_LOCATION, CONFIGURATION_PROFILES, PRINTERS, SERVICES, HARDWARE, LOCAL_USER_ACCOUNTS, CERTIFICATES, ATTACHMENTS, PLUGINS, PACKAGE_RECEIPTS, FONTS, SECURITY, OPERATING_SYSTEM, LICENSED_SOFTWARE, IBEACONS, SOFTWARE_UPDATES, EXTENSION_ATTRIBUTES, CONTENT_CACHING, GROUP_MEMBERSHIPS
+
+response=$(curl --request GET \
+	--url $url/api/v1/computers-inventory?section=$sections \
+	--header "Authorization: Bearer $bearerToken" \
+	--header "Accept: application/json")
+  
+echo $response
